9041c54e8abca5f88df58c6963585b46c9543e86
Seito <sktpiko@gmail.com>
2023-04-07 22:35:00 +0200

    slowly getting there with cunit

--- bellmanford_c/v2/tests/cunit.c
+++ bellmanford_c/v2/tests/cunit.c
@@ -13,8 +13,8 @@ Here are contained the tests to make sure the functions impelemented in the grap
 
 void test_get_file_info(void){
     //Default graph 
-    FILE * default_test = fopen("tests/CUnit/default.bin", "rb");
-    if (default_test == NULL){exit(1);}
+    FILE * default_test = fopen("tests/graph_bin/default.bin", "rb");
+    if (default_test == NULL){printf("default_test get file info"); exit(1);}
     graph_t * default_graph = get_file_info(default_test);
 
     CU_ASSERT_EQUAL(default_graph->file_infos->nb_edges, 10);
@@ -22,10 +22,11 @@ void test_get_file_info(void){
 
     free_graph_struct(default_graph);
     fclose(default_test);
+    printf("finished test_get_file_info");
 
     //Empty graph
-    FILE * empty_test = fopen("tests/CUnit/empty.bin", "rb");
-    if (empty_test == NULL){exit(1);}
+    FILE * empty_test = fopen("tests/graph_bin/empty.bin", "rb");
+    if (empty_test == NULL){printf("empty_test get file info"); exit(1);}
     graph_t * empty_graph = get_file_info(empty_test);
 
     CU_ASSERT_EQUAL(empty_graph, NULL);
@@ -34,8 +35,8 @@ void test_get_file_info(void){
     fclose(empty_test);
     
     //Corrupted graph
-    FILE * corrupted_test = fopen("tests/CUnit/corrupted.bin", "rb");
-    if (corrupted_test == NULL){exit(1);}
+    FILE * corrupted_test = fopen("tests/graph_bin/corrupted.bin", "rb");
+    if (corrupted_test == NULL){printf("corrupted_test get file info"); exit(1);}
     graph_t * corrupted_graph = get_file_info(corrupted_test);
 
     CU_ASSERT_EQUAL(corrupted_graph, NULL);
@@ -44,7 +45,7 @@ void test_get_file_info(void){
     fclose(corrupted_test);
     
     //Graph with negative cost
-    FILE * neg_test = fopen("tests/CUnit/graph_neg.bin", "rb");
+    FILE * neg_test = fopen("tests/graph_bin/graph_neg.bin", "rb");
     if (neg_test == NULL){
         printf("Error opening file");
         exit(1);
@@ -56,28 +57,27 @@ void test_get_file_info(void){
     fclose(neg_test);
     
     //Graph with only zeros
-    FILE * zero_test = fopen("tests/CUnit/graph_0.bin", "rb");
+    FILE * zero_test = fopen("tests/graph_bin/graph_0.bin", "rb");
     if (zero_test == NULL){
-        printf("Error opening file");
+        printf("Error opening file zero_test");
         exit(1);
     }
     graph_t * zero_graph = get_file_info(zero_test);
     CU_ASSERT_EQUAL(zero_graph, NULL);
     free_graph_struct(zero_graph);
     fclose(zero_test);
-    
 }
 
 void test_bellman_ford(void){
     //Default graph 
-    FILE * default_test = fopen("tests/CUnit/default.bin", "rb");
-    if (default_test == NULL){exit(1);}
+    FILE * default_test = fopen("tests/graph_bin/default.bin", "rb");
+    if (default_test == NULL){printf("def test bf");exit(1);}
 
     graph_t * default_graph = get_file_info(default_test);
-    if (default_graph == NULL) {exit(1);}
+    if (default_graph == NULL) {printf("def g bf");exit(1);}
 
     ford_t * result_def_1 = bellman_ford(default_graph->file_infos->nb_nodes, default_graph->file_infos->nb_edges, default_graph->graph_data, 0, false);
-    if (result_def_1 == NULL){exit(1);}
+    if (result_def_1 == NULL){printf("res def 1");exit(1);}
     
     //Asserts Equals here for node 0
     CU_ASSERT_EQUAL(result_def_1->dist[0], 0);
@@ -89,7 +89,7 @@ void test_bellman_ford(void){
     free_ford_struct(result_def_1);
 
     ford_t * result_def_2 = bellman_ford(default_graph->file_infos->nb_nodes, default_graph->file_infos->nb_edges, default_graph->graph_data, 4, false);
-    if (result_def_2 == NULL){exit(1);}
+    if (result_def_2 == NULL){printf("res def 2");exit(1);}
     
     //Asserts Equals for node 4
     CU_ASSERT_EQUAL(result_def_2->dist[0], 9);
@@ -104,14 +104,14 @@ void test_bellman_ford(void){
 
 
     //Graph with negative cost
-    FILE * neg_test = fopen("tests/CUnit/graph_neg.bin", "rb");
-    if (neg_test == NULL){exit(1);}
+    FILE * neg_test = fopen("tests/graph_bin/graph_neg.bin", "rb");
+    if (neg_test == NULL){printf("neg test bf");exit(1);}
 
     graph_t * neg_graph = get_file_info(neg_test);
-    if (neg_graph == NULL) {exit(1);}
+    if (neg_graph == NULL) {printf("neg graph bf");exit(1);}
 
     ford_t * result_def_3 = bellman_ford(neg_graph->file_infos->nb_nodes, neg_graph->file_infos->nb_edges, neg_graph->graph_data, 0, false);
-    if (result_def_3 == NULL){exit(1);}
+    if (result_def_3 == NULL){printf("res def 3");exit(1);}
 
     //Asserts Equals here for node 0
     CU_ASSERT_EQUAL(result_def_3->dist[0], 0);
@@ -121,7 +121,7 @@ void test_bellman_ford(void){
     free_ford_struct(result_def_3);
 
     ford_t * result_def_4 = bellman_ford(neg_graph->file_infos->nb_nodes, neg_graph->file_infos->nb_edges, neg_graph->graph_data, 2, false);
-    if (result_def_4 == NULL){exit(1);}
+    if (result_def_4 == NULL){printf("res def 4");exit(1);}
 
     //Asserts Equals here for node 2
     CU_ASSERT_EQUAL(result_def_4->dist[0], 13);
@@ -135,17 +135,17 @@ void test_bellman_ford(void){
 
 void test_get_max(void){
     //Default graph Setup
-    FILE * default_test = fopen("tests/CUnit/default.bin", "rb");
-    if (default_test == NULL){exit(1);}
+    FILE * default_test = fopen("tests/graph_bin/default.bin", "rb");
+    if (default_test == NULL){printf("def test max");exit(1);}
 
     graph_t * default_graph = get_file_info(default_test);
-    if (default_graph == NULL) {exit(1);}
+    if (default_graph == NULL) {printf("def g max");exit(1);}
 
     ford_t * result_def_5 = bellman_ford(default_graph->file_infos->nb_nodes, default_graph->file_infos->nb_edges, default_graph->graph_data, 1, false);
-    if (result_def_5 == NULL){exit(1);}
+    if (result_def_5 == NULL){printf("res def 5");exit(1);}
 
     mcost_t * max_def_1= get_max(default_graph->file_infos->nb_nodes,result_def_5->dist, default_graph->file_infos->nb_nodes);
-    if (max_def_1== NULL){exit(1);}
+    if (max_def_1== NULL){printf("max def 1");exit(1);}
 
     //Default graph Asserts
     CU_ASSERT_EQUAL(max_def_1->cost, 10);
@@ -159,17 +159,17 @@ void test_get_max(void){
 
 
     //Graph with negative cost
-    FILE * neg_test = fopen("tests/CUnit/graph_neg.bin", "rb");
-    if (neg_test == NULL){exit(1);}
+    FILE * neg_test = fopen("tests/graph_bin/graph_neg.bin", "rb");
+    if (neg_test == NULL){printf("neg t max");exit(1);}
     
     graph_t * neg_graph = get_file_info(neg_test);
-    if (neg_graph == NULL) {exit(1);}
+    if (neg_graph == NULL) {printf("neg g max");exit(1);}
     
     ford_t * result_def_6 = bellman_ford(neg_graph->file_infos->nb_nodes, neg_graph->file_infos->nb_edges, neg_graph->graph_data, 0, false);
-    if (result_def_6 == NULL){exit(1);}
+    if (result_def_6 == NULL){printf("res def 6");exit(1);}
     
     mcost_t * max_def_2 = get_max(neg_graph->file_infos->nb_nodes,result_def_6->dist, neg_graph->file_infos->nb_nodes);
-    if (max_def_2 == NULL){exit(1);}
+    if (max_def_2 == NULL){printf("max def 2");exit(1);}
 
     
     CU_ASSERT_EQUAL(max_def_2->cost, 8);
@@ -179,10 +179,10 @@ void test_get_max(void){
     free_ford_struct(result_def_6);
     
     ford_t * result_def_7 = bellman_ford(neg_graph->file_infos->nb_nodes, neg_graph->file_infos->nb_edges, neg_graph->graph_data, 2, false);
-    if (result_def_7 == NULL){exit(1);}
+    if (result_def_7 == NULL){printf("res def 7");exit(1);}
     
     mcost_t * max_def_3 = get_max(neg_graph->file_infos->nb_nodes,result_def_7->dist, neg_graph->file_infos->nb_nodes);
-    if (max_def_3 == NULL){exit(1);}
+    if (max_def_3 == NULL){printf("max def 3");exit(1);}
 
 
     CU_ASSERT_EQUAL(max_def_3->cost, 13);
@@ -196,21 +196,21 @@ void test_get_max(void){
 
 void test_get_path(void){
     //Default graph Setup
-    FILE * default_test = fopen("tests/CUnit/default.bin", "rb");
+    FILE * default_test = fopen("tests/graph_bin/default.bin", "rb");
     if (default_test == NULL){exit(1);}
     
     graph_t * default_graph = get_file_info(default_test);
     if (default_graph == NULL) {exit(1);}
     
     ford_t * result_def_8 = bellman_ford(default_graph->file_infos->nb_nodes, default_graph->file_infos->nb_edges, default_graph->graph_data, 1, false);
-    if (result_def_8 == NULL){exit(1);}
+    if (result_def_8 == NULL){printf("res def 8");exit(1);}
     
     mcost_t * max_def_4 = get_max(default_graph->file_infos->nb_nodes,result_def_8->dist, default_graph->file_infos->nb_nodes);
-    if (max_def_4 == NULL){exit(1);}
+    if (max_def_4 == NULL){printf("max def 4");exit(1);}
     
     int32_t size_def1 = default_graph->file_infos->nb_nodes;
     int32_t * path_def_1 = get_path(max_def_4->node, 1, result_def_8->path, &size_def1);
-    if (path_def_1 == NULL){printf("get path failed\n");exit(1);}
+    if (path_def_1 == NULL){printf("path_def_1\n");exit(1);}
 
     //Default graph Asserts
     CU_ASSERT_EQUAL(size_def1, 2);
@@ -226,21 +226,21 @@ void test_get_path(void){
 
 
     //Graph with negative cost
-    FILE * neg_test = fopen("tests/CUnit/graph_neg.bin", "rb");
+    FILE * neg_test = fopen("tests/graph_bin/graph_neg.bin", "rb");
     if (neg_test == NULL){exit(1);}
     
     graph_t * neg_graph = get_file_info(neg_test);
     if (neg_graph == NULL) {exit(1);}
     
     ford_t * result_def_9 = bellman_ford(neg_graph->file_infos->nb_nodes, neg_graph->file_infos->nb_edges, neg_graph->graph_data, 0, false);
-    if (result_def_9 == NULL){exit(1);}
+    if (result_def_9 == NULL){printf("res def 9");exit(1);}
     
     mcost_t * max_def_5 = get_max(neg_graph->file_infos->nb_nodes,result_def_9->dist, neg_graph->file_infos->nb_nodes);
-    if (max_def_5 == NULL){exit(1);}
+    if (max_def_5 == NULL){printf("max def 5");exit(1);}
     
     int32_t size_def2 = default_graph->file_infos->nb_nodes;
     int32_t * path_def_2 = get_path(max_def_5->node, 0, result_def_9->path, &size_def2);
-    if (path_def_2 == NULL){printf("get path failed\n");exit(1);}
+    if (path_def_2 == NULL){printf("path_def_2\n");exit(1);}
 
 
     CU_ASSERT_EQUAL(size_def2, 3);
@@ -253,14 +253,14 @@ void test_get_path(void){
     free_ford_struct(result_def_9);
     
     ford_t * result_def_10 = bellman_ford(neg_graph->file_infos->nb_nodes, neg_graph->file_infos->nb_edges, neg_graph->graph_data, 2, false);
-    if (result_def_10 == NULL){exit(1);}
+    if (result_def_10 == NULL){printf("res def 10");exit(1);}
     
     mcost_t * max_def_6 = get_max(neg_graph->file_infos->nb_nodes,result_def_10->dist, neg_graph->file_infos->nb_nodes);
-    if (max_def_6 == NULL){exit(1);}
+    if (max_def_6 == NULL){printf("max_def_6");exit(1);}
     
     int32_t size_def3 = default_graph->file_infos->nb_nodes;
     int32_t * path_def_3 = get_path(max_def_6->node, 2, result_def_10->path, &size_def3);
-    if (path_def_3 == NULL){printf("get path failed\n");exit(1);}
+    if (path_def_3 == NULL){printf("path_def_3\n");exit(1);}
 
     
     CU_ASSERT_EQUAL(size_def3, 3);
@@ -293,6 +293,7 @@ int main(){
         return CU_get_error();
     }
     CU_basic_run_tests();
+    printf("hey");
     CU_basic_show_failures(CU_get_failure_list());
     CU_cleanup_registry();
 }
