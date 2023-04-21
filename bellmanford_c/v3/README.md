32f4d23e56c67bf4f075bd18bc6286167b33463d
Seito1090 <sktpiko@gmail.com>
2023-04-21 20:50:22 +0200

    v3 started

---
+++ bellmanford_c/v3/README.md
@@ -0,0 +1,36 @@
+# Readme_Groupe_M4
+
+## Algorithme de Bellman-Ford en C
+
+***
+
+Le programme de ce projet implemente l'algorithme de Bellman-Ford, il nous permet de trouver le chemin le plus court / moins coûteu entre 2 noeuds d'un graphe. 
+Il est base sur un code python qui nous a ete fourni
+
+### Comment lancer le programme
+
+Il faut avoir les outils/librairies suivantes pour eviter tout soucis de compilation : CUnit, Valgrind, GCC 
+
+Pour lancer le programme, il faut d'abord le compiler, voici quelques commandes utiles pour le faire :
+ - **make** / **make sp** : Permet de compiler le programme de façon à ce qu'il puisse être utilise, cela cree un executable 'main' que vous pouvez executer, pour plus d'infos utiliser ./main -h
+ - **make tests** : Permet d'executer une serie de tests pour s'assurer que le programme fonctionne bien comme attendu, ceci compile les tests CUnit et les fait passer par valgrind pour vous assurer qu'aucune fuite de memoire est presente, ensuite le programme main est compile si ce n'est pas encore fait et fait passer main par valgrind pour verifier les fuites de memoire.
+ - **make clean** : Supprime tout les fichiers supplementaires qui ont etes crees lors de l'utilisation des 2 commandes precedentes 
+
+Pour des soucis de clarete utiliser toujours **make clean** après avoir fini d'utiliser le programme, le makefile fait en sorte que peu importe l'ordre du **make/make sp/make tests**, il ne soit pas necessaire de faire autre chose.  
+
+### Utilisation de la fonction main 
+
+La fonction main prend comme arguments : 
+ - *-n* : Le nombre de threads que le programme va utiliser 
+ - *-v* : Affichage des messages d'erreur, false par defaut
+ - *-f* : Le fichier dans lequel les donnee seront sauvegardees
+ - *-?* / *-h* : Affiche une fenetre avec cette explication 
+ - Le nom du fichier avec le graph
+
+### Utilisation de graph personnels
+
+Ce projet vous permet de créer votre propre graphe, d'executer le programme sur ce graph et de vérifier que cette réponse est correcte. Vous avez 2 possibilites pour le faire :
+ - Vous avez deja votre fichier binaire ? Pas de soucis vous n'avez qu'a executer le programme main avec le chemin vers votre fichier comme parametre, ensuite une fois fait, utilisez la commande : python tests/necessary_python/check_graph.py *chemin vers votre fichier binaire*, pour finir lancez le programme cunit_tests avec les 2 outputs comme parametres et il vous confirmera si les 2 fichiers outputs sont identiques ou non.
+ - Si vous n'avez pas de fichier binaire ? Vous pouvez utiliser le code python qui a ete fourni pour en creer un ! Vous autrez besoin de rentrer votre graph dans un fichier ntf avec une structure : *node_from node_to cost* (voir les exemples dans le dossier tests/ntf_files) et ensuite executer le code python avec la ligne suivante : python tests/necessary_python/create_graph.py *chemin vers fichier*. Une fois fait, executez le programme main normalement et suiver les instructions du point precedent.
+
+Vos fichiers ne seront par contre pas supprimes si vous utilisez la commande **make clean**.
---
+++ bellmanford_c/v3/tests/cunittests.c
@@ -0,0 +1,305 @@
+/*
+Here are contained the tests to make sure the functions impelemented in the graph.c file are working properly.
+*/
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/graph.h"
+
+// Test Suite setup and cleanup functions:
+
+/*
+Function test_default
+--------------------------------
+Tests the code with the default graph, 5 nodes, 10 links, seed 42
+*/
+void test_default(void){
+    /*Tests the code with the default graph, 5 nodes, 10 links, seed 42*/
+    FILE * default_file = fopen("tests/graph_bin/default.bin", "rb");
+    if (default_file == NULL){printf("default_test get file info"); exit(1);}
+
+    graph_t * default_graph = get_file_info(default_file);
+    
+    CU_ASSERT_EQUAL(default_graph->file_infos->nb_edges, 10);
+    CU_ASSERT_EQUAL(default_graph->file_infos->nb_nodes, 5);
+
+    ford_t * result_node_0 = bellman_ford(default_graph->file_infos->nb_nodes, default_graph->file_infos->nb_edges, default_graph->graph_data, 0, false);
+    if (result_node_0 == NULL){printf("res def 1");exit(1);}
+    
+    //Asserts Equals here for node 0
+    CU_ASSERT_EQUAL(result_node_0->dist[0], 0);
+    CU_ASSERT_EQUAL(result_node_0->dist[1], 4);
+    CU_ASSERT_EQUAL(result_node_0->dist[2], 4);
+    CU_ASSERT_EQUAL(result_node_0->dist[3], 14);
+    CU_ASSERT_EQUAL(result_node_0->dist[4], 7);
+
+    free_ford_struct(result_node_0);
+
+    ford_t * result_node_4 = bellman_ford(default_graph->file_infos->nb_nodes, default_graph->file_infos->nb_edges, default_graph->graph_data, 4, false);
+    if (result_node_4 == NULL){printf("res def 2");exit(1);}
+    
+    //Asserts Equals for node 4
+    CU_ASSERT_EQUAL(result_node_4->dist[0], 9);
+    CU_ASSERT_EQUAL(result_node_4->dist[1], 13);
+    CU_ASSERT_EQUAL(result_node_4->dist[2], 13);
+    CU_ASSERT_EQUAL(result_node_4->dist[3], 23);
+    CU_ASSERT_EQUAL(result_node_4->dist[4], 0);
+
+    free_ford_struct(result_node_4);
+
+    ford_t * result_node_1 = bellman_ford(default_graph->file_infos->nb_nodes, default_graph->file_infos->nb_edges, default_graph->graph_data, 1, false);
+    if (result_node_1 == NULL){printf("res def 5");exit(1);}
+
+    mcost_t * max_node_1= get_max(default_graph->file_infos->nb_nodes,result_node_1->dist, 1);
+    if (max_node_1== NULL){printf("max def 1");exit(1);}
+
+    //Default graph Asserts
+    CU_ASSERT_EQUAL(max_node_1->cost, 10);
+    CU_ASSERT_EQUAL(max_node_1->node, 3); 
+
+    //Default graph Free
+    free_max_struct(max_node_1);
+    free_ford_struct(result_node_1);
+
+    ford_t * result_node_2 = bellman_ford(default_graph->file_infos->nb_nodes, default_graph->file_infos->nb_edges, default_graph->graph_data, 2, false);
+    if (result_node_2 == NULL){printf("res def 8");exit(1);}
+    
+    mcost_t * max_node_2 = get_max(default_graph->file_infos->nb_nodes,result_node_2->dist, 1);
+    if (max_node_2 == NULL){printf("max def 4");exit(1);}
+    
+    int32_t size_node_2 = default_graph->file_infos->nb_nodes;
+    int32_t * path_node_2 = get_path(max_node_2->node, 2, result_node_2->path, &size_node_2);
+    
+    //Default graph Asserts
+    CU_ASSERT_EQUAL(size_node_2, 4);
+    CU_ASSERT_EQUAL(path_node_2[0], 2);
+    CU_ASSERT_EQUAL(path_node_2[1], 0); 
+    CU_ASSERT_EQUAL(path_node_2[2], 1);
+    CU_ASSERT_EQUAL(path_node_2[3], 3);
+
+    //Default graph Free
+    free_path(path_node_2);
+    free_max_struct(max_node_2);
+    free_ford_struct(result_node_2);
+    free_graph_struct(default_graph);
+}
+
+/*
+Function test_negative_cost
+--------------------------------
+Tests the code with the negative cost graph, 3 nodes, 4 links, seed 42
+*/
+void test_negative_cost(void){
+    FILE * neg_test = fopen("tests/graph_bin/neg_cost.bin", "rb");
+    if (neg_test == NULL){
+        printf("Error opening file");
+        exit(1);
+    }
+    graph_t * neg_graph = get_file_info(neg_test);
+
+    CU_ASSERT_EQUAL(neg_graph->file_infos->nb_edges, 4);
+    CU_ASSERT_EQUAL(neg_graph->file_infos->nb_nodes, 3);
+
+    ford_t * result_neg_node_2 = bellman_ford(neg_graph->file_infos->nb_nodes, neg_graph->file_infos->nb_edges, neg_graph->graph_data, 2, false);
+    if (result_neg_node_2 == NULL){printf("res def 4");exit(1);}
+
+    //Asserts Equals here for node 2
+    CU_ASSERT_EQUAL(result_neg_node_2->dist[0], 13);
+    CU_ASSERT_EQUAL(result_neg_node_2->dist[1], 9);
+    CU_ASSERT_EQUAL(result_neg_node_2->dist[2], 0);
+    
+    ford_t * result_neg_node_0 = bellman_ford(neg_graph->file_infos->nb_nodes, neg_graph->file_infos->nb_edges, neg_graph->graph_data, 0, false);
+    if (result_neg_node_0 == NULL){printf("res def 3");exit(1);}
+
+    //Asserts Equals here for node 0
+    CU_ASSERT_EQUAL(result_neg_node_0->dist[0], 0);
+    CU_ASSERT_EQUAL(result_neg_node_0->dist[1], 8);
+    CU_ASSERT_EQUAL(result_neg_node_0->dist[2], -1);
+
+    mcost_t * max_neg_node_0 = get_max(neg_graph->file_infos->nb_nodes,result_neg_node_0->dist, 0);
+    if (max_neg_node_0 == NULL){printf("max def 2");exit(1);}
+
+    CU_ASSERT_EQUAL(max_neg_node_0->cost, 8);
+    CU_ASSERT_EQUAL(max_neg_node_0->node, 1); 
+
+    mcost_t * max_neg_node_2 = get_max(neg_graph->file_infos->nb_nodes,result_neg_node_2->dist, 2);
+    if (max_neg_node_2 == NULL){printf("max def 3");exit(1);}
+
+    CU_ASSERT_EQUAL(max_neg_node_2->cost, 13);
+    CU_ASSERT_EQUAL(max_neg_node_2->node, 0); 
+    
+    int32_t size_neg_node_0 = neg_graph->file_infos->nb_nodes;
+    int32_t * path_neg_node_0 = get_path(max_neg_node_0->node, 0, result_neg_node_0->path, &size_neg_node_0);
+    if (path_neg_node_0 == NULL){printf("path_neg_node_0\n");exit(1);}
+
+    CU_ASSERT_EQUAL(size_neg_node_0, 3);
+    CU_ASSERT_EQUAL(path_neg_node_0[0], 0);
+    CU_ASSERT_EQUAL(path_neg_node_0[1], 2); 
+    CU_ASSERT_EQUAL(path_neg_node_0[2], 1);
+
+    int32_t size_neg_node_2 = neg_graph->file_infos->nb_nodes;
+    int32_t * path_neg_node_2 = get_path(max_neg_node_2->node, 2, result_neg_node_2->path, &size_neg_node_2);
+    if (path_neg_node_2 == NULL){printf("path_def_3\n");exit(1);}
+
+    CU_ASSERT_EQUAL(size_neg_node_2, 3);
+    CU_ASSERT_EQUAL(path_neg_node_2[0], 2);
+    CU_ASSERT_EQUAL(path_neg_node_2[1], 1); 
+    CU_ASSERT_EQUAL(path_neg_node_2[2], 0);
+
+    //Free Negative graph
+    free_path(path_neg_node_2);
+    free_path(path_neg_node_0);
+    free_max_struct(max_neg_node_0);
+    free_max_struct(max_neg_node_2);
+    free_ford_struct(result_neg_node_2);
+    free_ford_struct(result_neg_node_0);
+    free_graph_struct(neg_graph);
+}
+
+/*
+Function test_empty
+--------------------------------
+Tests the code with an edge case, an empty graph
+*/
+void test_empty(void){
+    FILE * empty_test = fopen("tests/graph_bin/empty.bin", "rb");
+    if (empty_test == NULL){printf("empty_test get file info"); exit(1);}
+
+    graph_t * empty_graph = get_file_info(empty_test);
+
+    CU_ASSERT_EQUAL(empty_graph, NULL);
+    
+}
+
+/*
+Function test_corrupted
+--------------------------------
+Tests the code with an edge case, a corrupted graph
+*/
+void test_corrupted(void){
+    FILE * corrupted_test = fopen("tests/graph_bin/corrupted.bin", "rb");
+    if (corrupted_test == NULL){printf("corrupted_test get file info"); exit(1);}
+    
+    graph_t * corrupted_graph = get_file_info(corrupted_test);
+
+    CU_ASSERT_EQUAL(corrupted_graph, NULL);
+}
+
+/*
+Function test_only_zeros
+--------------------------------
+Tests the code with an edge case, a graph with only zeros
+*/
+void test_only_zeros(void){
+    FILE * zero_test = fopen("tests/graph_bin/zeros.bin", "rb");
+    if (zero_test == NULL){
+        printf("Error opening file zero_test");
+        exit(1);
+    }
+    graph_t * zero_graph = get_file_info(zero_test);
+
+    CU_ASSERT_EQUAL(zero_graph, NULL);
+}
+
+/*
+Function check_binary_files_equal
+--------------------------------
+This function checks if two binary files are equal
+
+Input:
+---------
+const char* filename1: The name of the first file
+const char* filename2: The name of the second file
+
+Output:
+---------
+int: 0 if the files are equal, 1 otherwise
+*/
+int check_binary_files_equal(const char* filename1, const char* filename2) {
+    FILE* file1 = fopen(filename1, "rb");
+    if (!file1) {
+        printf("Error: Failed to open file %s\n", filename1);
+        return 1;
+    }
+
+    FILE* file2 = fopen(filename2, "rb");
+    if (!file2) {
+        printf("Error: Failed to open file %s\n", filename2);
+        fclose(file1);
+        return 1;
+    }
+
+    int is_equal = 1;
+    uint8_t byte1, byte2;
+    while (1) {
+        byte1 = fgetc(file1);
+        byte2 = fgetc(file2);
+        if (byte1 != byte2) {
+            is_equal = 0;
+            break;
+        }
+        if (feof(file1) && feof(file2)) {
+            break;
+        }
+        if (feof(file1) || feof(file2)) {
+            is_equal = 0;
+            break;
+        }
+    }
+
+    if (!is_equal) {
+        printf("The files are not equal\n");
+        fclose(file1);
+        fclose(file2);
+        return 1;
+    }
+
+    fclose(file1);
+    fclose(file2);
+
+    printf("The files are equal\n");
+    return 0;
+}
+
+/*
+Function test_check_binary_files_equal
+--------------------------------
+This function tests cases for the check_binary_files_equal function
+*/
+void test_check_binary_files_equal(void) {
+    CU_ASSERT_EQUAL(check_binary_files_equal("tests/output_tests/default_py.bin", "tests/output_tests/default_c.bin"), 0);
+    CU_ASSERT_EQUAL(check_binary_files_equal("tests/output_tests/neg_py.bin", "tests/output_tests/neg_c.bin"), 0);
+    CU_ASSERT_EQUAL(check_binary_files_equal("tests/output_tests/wrong_py.bin", "tests/output_tests/default_c.bin"), 1);
+    CU_ASSERT_EQUAL(check_binary_files_equal("tests/output_tests/non_existent_file.bin", "tests/output_tests/default_c.bin"), 1);
+}
+
+int main(int argc, char **argv){
+    if (CUE_SUCCESS != CU_initialize_registry())
+        return CU_get_error();
+    CU_pSuite pSuite = NULL;
+    pSuite = CU_add_suite("Suite_1", NULL, NULL);
+    if (NULL == pSuite) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+    if (argc > 1){
+        printf("Test de correspondance d'un graph personalise\n"); 
+        check_binary_files_equal(argv[1], argv[2]); 
+    } else {
+        if ((NULL == CU_add_test(pSuite, "test of default graph", test_default)) ||
+            (NULL == CU_add_test(pSuite, "test of graph with negative cost", test_negative_cost)) ||
+            (NULL == CU_add_test(pSuite, "test of empty graph", test_empty)) ||
+            (NULL == CU_add_test(pSuite, "test of corrupted graph", test_corrupted)) ||
+            (NULL == CU_add_test(pSuite, "test of full zeros graph", test_only_zeros)) || 
+            (NULL == CU_add_test(pSuite, "test of binary output files", test_check_binary_files_equal))
+            ){
+            CU_cleanup_registry();
+            return CU_get_error();
+        }
+        CU_basic_run_tests();
+        CU_basic_show_failures(CU_get_failure_list());
+    }
+    CU_cleanup_registry();
+}
